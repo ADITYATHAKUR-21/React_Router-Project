db83f501e68aec8bc4b51d7ffccd0be7d116c8ed
Aditya Kumar <adityakr123muz@gmail.com>
2025-08-03 23:25:50 +0530

    Added Layout , About, & Contact

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import Layout from './Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
 
-
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Layout/>,
+    children: [
+      {
+        path : '',
+        element : <Home/>
+      },
+      {
+        path : 'about',
+        element : <About/>
+      },
+      {
+        path : 'contact',
+        element : <Contact/>
+      }
+    ]
+  }
+])
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={''}/>
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
